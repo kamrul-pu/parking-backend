1b1b0825893a6c3564d7d4ff74547c0b79a6a982
Kamrul Hasan <kamrul.h456@gmail.com>
2024-12-02 16:05:52 +0600

    build sh file modified

--- app/build.sh
+++ app/build.sh
@@ -6,7 +6,3 @@ pip install -r requirements/dev.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[ $CREATE_SUPERUSER ]];
-then 
-    python app/manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi
